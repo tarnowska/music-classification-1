131ee5777bc02a152fe3610e6739b28be5f0a6de
Araav Patel <araavp@gmail.com>
2020-04-28 02:54:48 -0700

    fixed variant1

--- music-classification/main.py
+++ music-classification/main.py
@@ -1,11 +1,3 @@
-# TO DO
-"""
-THE SECOND VARIANT IS COMPLETELY DONE (HOPEFULLY)
-THE FIRST VARIANT NEEDS TO SHOW THE WINDOWS AS ROWS AND THE FEATURES AS COLUMNS -- LOOK AT THE EXAMPLE WHEN IT IS
-GRAPHED
-"""
-
-
 # Imports
 import pathlib
 from pathlib import Path
@@ -16,15 +8,21 @@ import convert_mp3_to_wav as convert
 # Declare variables
 first_time = True
 ## If you want first_variant keep True, if you want second variant, make False
-first_variant = True
+first_variant = False
+## File path to folder where the audio files are located
+file_path = "dataset/audio/MEMD_audio"
+
 
 # Converts all mp3 files to wav files
-convert.convert_dir_mp3_to_wav("test_audio", 16000, 1)
+convert.convert_dir_mp3_to_wav(file_path, 16000, 1)
 
 
 # Loops through file with audio
-for file in pathlib.Path("test_audio").iterdir():
+for file in pathlib.Path(file_path).iterdir():
+    # Finds the type of file
     file_type = os.path.splitext(str(file))[-1].lower()
+
+    # Only processes if it is a .wav file
     if file_type == ".wav":
         # Reads audio file
         [Fs, x] = stfe.read_audio_file(file)
@@ -35,20 +33,35 @@ for file in pathlib.Path("test_audio").iterdir():
         # Extracts features
         if first_variant:
                 F, f_names = stfe.feature_extraction(x, Fs, 0.500 * Fs, 0.025 * Fs)
+                # Creates individual csv file
                 csv_name = (Path(file).stem + "_st_features.csv")
         else:
-            F, f_names = stfe.feature_extraction(x, Fs, 0.500 * Fs, 32.0 * Fs)
+            F, f_names = stfe.feature_extraction(x, Fs, 0.500 * Fs, 45.0 * Fs)
 
         # Extracts short term features
         short_term_features, short_term_features_names = stfe.extract_short_term_features(F, f_names)
 
+        if first_variant:
+            # Flips rows and cols
+            short_term_features = [[short_term_features[j][i] for j in range(len(short_term_features))] for i in range(len(short_term_features[0]))]
+            # Adds windows to short_term_features
+            window = 0
+            # Determines each window's length
+            window_length = (45 / len(F[0]))
+
+            # Adds windows in rows
+            for i in range(len(short_term_features)):
+                short_term_features[i].insert(0, window)
+                window += window_length
+
         # Creates csv file
-        stfe.create_csv(csv_name, file, short_term_features_names, short_term_features, first_time)
+        stfe.create_csv(csv_name, file, short_term_features_names, short_term_features, first_variant, first_time)
 
         # Signals that next loop is not the first one - helps with editing csv file
         if first_variant is False:
             first_time = False
 
+    # Runs else if file is not type .wav
     else:
         # Get rid of this if you want to keep the original mp3 files
         os.remove(str(file))
--- music-classification/short_term_feature_extractor.py
+++ music-classification/short_term_feature_extractor.py
@@ -1,13 +1,8 @@
+# Imports
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import ShortTermFeatures
 from pathlib import Path
 import csv
-
-import matplotlib.pyplot as plt
-from numpy import asarray
-from numpy import savetxt
-import numpy as np
-import pandas as pd
 
 
 # Reads wav file
@@ -28,22 +23,34 @@ def extract_short_term_features(features, features_names):
     short_features_names = []
     for x in range(0, 34):
         # Creates new array of only short term features and respective names
-        short_features.append(features[x][0])
+        short_features.append(features[x])
         short_features_names.append(features_names[x])
     return short_features, short_features_names
 
 
 # Creates csv file for short term features
-def create_csv(csv_file_name, file_audio_name, short_features_names, short_features, first_time):
+def create_csv(csv_file_name, file_audio_name, short_features_names, short_features, variant, first_time):
+    # Adds feature names to array
     short_features_names.insert(0, "Short Term Feature Names: ")
-    short_features.insert(0, Path(file_audio_name).stem)
+    if variant is False:
+        # Adds name of each audio file
+        short_features.insert(0, Path(file_audio_name).stem)
     if first_time:
+        # Writes a file with the name
         with open(csv_file_name, 'w') as myFile:
             file_writer = csv.writer(myFile)
+            # Adds feature names as first row
             file_writer.writerow(short_features_names)
-            file_writer.writerow(short_features)
+            if variant:
+                for i in range(len(short_features)):
+                    file_writer.writerow(short_features[i])
+            else:
+                file_writer.writerow(short_features)
     else:
         with open(csv_file_name, 'a') as myFile:
             file_writer = csv.writer(myFile)
-            file_writer.writerow(short_features)
-
+            if variant:
+                for i in range(len(short_features)):
+                    file_writer.writerow(short_features[i])
+            else:
+                file_writer.writerow(short_features)
